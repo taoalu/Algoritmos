8af5609d0aea4dc482d2979d77b18dbeb82df63b
taoalu <thi15ago87>
2016-09-05 16:55:29 -0300

    Versão 1.0 - Enviada 05/09/2016

---
+++ Exercicios_Algoritmos/src/exercicios_algoritmos/FuncoesLista.java
@@ -0,0 +1,36 @@
+package exercicios_algoritmos;
+public class FuncoesLista {
+
+    int remover(int[] array, int fimDoArray) {
+        int valorRemovido = array[fimDoArray];
+        array[fimDoArray] = 0;
+        return valorRemovido;
+    }
+
+    boolean inserirNoInicio(int[] array, int valor) {
+        if (array.length == 0) {
+            return false;
+        } else {
+            int aux;
+            for (int i = 0; i < array.length; i++) {
+                aux = array[i];
+                array[i] = aux;
+            }
+            array[0] = valor;
+            return true;
+        }
+    }
+
+    void imprimirLista(int[] array) {
+        if (array.length == 0) {
+            System.out.println("Array vazio!");
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] != 0) {
+                    System.out.print(array[i] + " ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
---
+++ Exercicios_Algoritmos/src/exercicios_algoritmos/TesteFuncoes.java
@@ -0,0 +1,17 @@
+package exercicios_algoritmos;
+
+public class TesteFuncoes {
+    public static void main(String[] args) {
+    //Testando o GitHub
+        FuncoesLista m = new FuncoesLista();
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int inseridos = array.length - 1;
+        
+        m.imprimirLista(array);
+        m.inserirNoInicio(array, 10);
+        m.imprimirLista(array);
+        System.out.println("Valor removido: " + m.remover(array, inseridos));
+        m.imprimirLista(array);
+    }
+}
