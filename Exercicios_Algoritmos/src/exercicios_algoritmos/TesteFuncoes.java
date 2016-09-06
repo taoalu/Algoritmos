066e8b84157035c45f17d0d8f24650fe2257637f
taoalu <thi15ago87>
2016-09-05 21:36:41 -0300

    Versão 1.1 - Adicionei mais métodos - 05/09/2016

--- Exercicios_Algoritmos/src/exercicios_algoritmos/TesteFuncoes.java
+++ Exercicios_Algoritmos/src/exercicios_algoritmos/TesteFuncoes.java
@@ -9,12 +9,16 @@ public class TesteFuncoes {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int inseridos = array.length - 1;
 
-        m.imprimirLista(array);
-        m.inserirNoInicio(array, 10);
-        m.imprimirLista(array);
+        m.imprimirLista(array);       
         System.out.println("Valor removido do final: " + m.remover(array, inseridos));
         m.imprimirLista(array);
         System.out.println("Valor removido do incício: " + m.removerDoInicio(array));
+        m.imprimirLista(array);
+         System.out.println("Valor inserido em qualquer posição");
+        m.inserirEmQualquerPosicao(array, 4, 333);
+        m.imprimirLista(array);
+        System.out.println("Valor inserido no início");
+        m.inserirNoInicio(array, 333);
         m.imprimirLista(array);
     }
 }
